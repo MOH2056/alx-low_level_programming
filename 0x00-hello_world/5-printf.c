3265c763acb94bebc32c030b683a984612ef205c
MOH2056 <tapereyetunde@gmail.com>
2023-08-10 15:29:39 -0700

    number5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - this is a function
- *
  * Return: always 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
-}	
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
