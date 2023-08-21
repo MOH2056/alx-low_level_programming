8ba8d18c8fad5d3601f24605cf1b949bc0a6b4e2
MOH2056 <tapereyetunde@gmail.com>
2023-08-21 10:10:19 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts - print every other character
+ * @str: parameter
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
