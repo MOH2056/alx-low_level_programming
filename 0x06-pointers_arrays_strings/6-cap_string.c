b80033e2972b135286ef84f46b0fec61bd14a96d
MOH2056 <tapereyetunde@gmail.com>
2023-08-24 17:05:52 -0700

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,7 +22,7 @@ char *cap_string(char *str)
 			str[index - 1] == '.' ||
 			str[index - 1] == '!' ||
 			str[index - 1] == '?' ||
-			str[index - 1] == " " ||
+			str[index - 1] == '"' ||
 			str[index - 1] == '(' ||
 			str[index - 1] == ')' ||
 			str[index - 1] == '{' ||
