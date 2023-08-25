48984d8da7d5086f6af1332d82d2ca824cb6f279
MOH2056 <tapereyetunde@gmail.com>
2023-08-25 08:33:34 -0700

    task11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void print_number(int n);
 char *rot13(char *);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
