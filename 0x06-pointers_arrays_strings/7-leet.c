4eb3a42ab378b732953c1524a43b2427218115cb
MOH2056 <tapereyetunde@gmail.com>
2023-08-24 17:31:16 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *leet - this function is to encode
+ * @n: parameter
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTIL";
+	char s2[] = "4433007711";
+
+	for (i = 0 ; n[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
