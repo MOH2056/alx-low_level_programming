16af20225ed1c428de3093bd64ea5943c1fe161d
MOH2056 <tapereyetunde@gamil.com>
2023-09-12 15:44:07 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - this is a dog struct
+ * @name: parameter1
+ * @age: parameter2
+ * @owner: parameter3
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
-void init_dog(struct dog *d,char*name,float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_do(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
