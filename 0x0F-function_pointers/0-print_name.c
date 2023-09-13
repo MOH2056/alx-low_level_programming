f8d262b9dbcd2baf8ca0dfd55b3bd6c72458adf8
MOH2056 <tapereyetunde@gamil.com>
2023-09-13 14:32:23 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - this function prints name
+ * @name: parameter
+ * @f: pointer
+ * Return: always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+project pn functin pointers
