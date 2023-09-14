dee87a2a96bff9cd37fe861d90e1f89b308bebdd
MOH2056 <tapereyetunde@gamil.com>
2023-09-14 15:53:59 -0700

    task0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,10 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTION_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
 
 #endif
