e145acc6685c376eb2569a28b3e4270a787e2efb
MOH2056 <tapereyetunde@gamil.com>
2023-09-26 16:45:50 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+* free_listint - frees a linked list
+* @head: listint_t list to be freed
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
