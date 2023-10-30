36de731c790c93f687368f7627f94a96256aedce
MOH2056 <tapereyetunde@gmail.com>
2023-10-30 14:17:21 -0700

    number

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - this is a function
- *
+ * @x: parameter
+ * @y: parameter
  * Return: 0
  */
 int main(int x, char *y[])
 {
 	if (x <= 4)
 		printf("%s\n", y[0]);
-	return 0;
+	return (0);
 }
