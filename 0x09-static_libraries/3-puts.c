5000796835ffe429265ced3316a998971a1bad42
MOH2056 <tapereyetunde@gmail.com>
2023-10-31 01:19:58 -0700

    number001

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - this is a function
  * @s: parameter
@@ -6,4 +7,9 @@
  */
 void _puts(char *s)
 {
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		putchar(s[i]);
+	putchar('\n');
 }
