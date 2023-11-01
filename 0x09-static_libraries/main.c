4821b78a88b77b500d019385c33d234122565b22
MOH2056 <tapereyetunde@gmail.com>
2023-11-01 13:26:47 -0700

    figure

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
-
+/**
+ * main - this is a function
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was tobe the best/ hacker\"\n");
+	_puts("\t- Kevin Mitnick");
+	return (0);
 }
