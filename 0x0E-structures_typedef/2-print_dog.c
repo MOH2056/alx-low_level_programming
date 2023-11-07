c3959afc53a53d52651670b4deea909b11236635
MOH2056 <tapereyetunde@gmail.com>
2023-11-07 08:22:59 -0800

    figuree2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,14 +3,13 @@
 /**
  * print_dog - this is a function
  * @d: parameter
- * Return: 0
  */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf("age: %f\n", (d->age) ? d->age : 0);
+		printf("Age: %f\n", (d->age) ? d->age : 0);
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
