411403907620429595a153ef9c38857ace14a429
MOH2056 <tapereyetunde@gmail.com>
2023-11-07 07:21:41 -0800

    figure1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include "dog.h"
 /**
  * init_dog - this is a function
  * @d: pointer
  * @name:pointer 1
  * @age: parameter
  * @owner: parameter2
- * Return: o
+ * Return:0
  */
-struct dog {
-	char *name;
-	float age;
-	char *owner;
-};
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strncpy(d->name, name, sizeof(d->name) - 1);
+	strncpy(d->name, name,sizeof(d->name) - 1);
 	d->age = age;
-	strncpy(d->owner, owner, sizeof(d->owner) - 1);
+	strncpy(d->owner, owner, sizeof(d->name) - 1);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* definig the template of do struct*/
-/*struct dog{
+/**
+ * struct dog - this is a dta structure
+ * @name: parameter1
+ * @age: parameter2
+ * @owner: parameter3
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};*/
+} dog_t;
 
-typedef struct dog {
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
-
-/*other prototype*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
