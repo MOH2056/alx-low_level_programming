f6ef4dec685c9be2ffc04b5598f6aefdfdf6f8e3
MOH2056 <tapereyetunde@gmail.com>
2023-11-08 11:09:53 -0800

    number01

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t a;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (a = 0; a < size; a++)
 		action(array[a]);
 }
