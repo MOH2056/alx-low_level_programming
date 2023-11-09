c6711d7970c3330aa8d251c73fe23b06607e7ef9
MOH2056 <tapereyetunde@gmail.com>
2023-11-09 06:43:53 -0800

    task0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - this is a function
+ * @n: parameter
+ * Return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int result = 0;
+	unsigned int a;
+	va_list number;
+
+	va_start(number, n);
+
+	/*unsigned int result = 0;
+	unsigned int a;*/
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (a = 0; a < n; a++)
+	{
+		result = result + va_arg(number, unsigned int);
+	}
+	va_end(number);
+	return (result);
+}
