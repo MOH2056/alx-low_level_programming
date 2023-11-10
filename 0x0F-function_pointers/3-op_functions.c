3f35a7b42d0011ab27e605f6264772fcf1cd7f16
MOH2056 <tapereyetunde@gmail.com>
2023-11-10 15:58:00 -0800

    task03

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - this is function
+ * @a: parameter
+ * @b: parameter1
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - this is a function
+ * @a: parameter1
+ * @b: parameter2
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - this is a function
+ * @a: parameter
+ * @b: parameter2
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - this is a function
+ * @a: parameter
+ * @b: parameter2
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - this is a function
+ * @a: parameter1
+ * @b: parameter2
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
