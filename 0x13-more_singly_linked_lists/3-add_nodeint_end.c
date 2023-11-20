c2721e4fa9d7181a2c936d5dd62919ca93b96256
MOH2056 <tapereyetunde@gmail.com>
2023-11-20 13:38:43 -0800

    task5

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,12 +11,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *end;
 
 	newone = (listint_t *)malloc(sizeof(listint_t));
+	if (newone == NULL)
+	{
+		return (NULL);
+	}
 	newone->n = n;
 	newone->next = NULL;
 	if (*head == NULL)
 	{
 		*head = newone;
-		return (newone);
 	}
 	while (end->next != NULL)
 		end = end->next;
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
 	listint_t *next;
-	
+
 	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
-	
 	*head = NULL;
 }
