64da3c074fa30d704546d5c3e69cadf2d6ae6784
MOH2056 <tapereyetunde@gmail.com>
2023-11-20 08:28:59 -0800

    task2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint - function
+ * @head: parameter
+ * @n: parameter1
+ * Return: 0
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newone;
+
+	newone = (listint_t *)malloc(sizeof(listint_t));
+
+	if (newone == NULL)
+	{
+		return (NULL);
+	}
+	newone->n = n;
+	newone->next = *head;
+	*head = newone;
+	return (0);
+}
