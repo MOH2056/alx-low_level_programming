dc762302c3b15d0c88aca80a744d0c1d8268123d
MOH2056 <tapereyetunde@gmail.com>
2023-11-20 07:13:17 -0800

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - this is a function
+ * @h: paramster
+ * Return: 0
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
