010e8017cceed4d93b8fb222a6db462e5f0c2958
MOH2056 <tapereyetunde@gmail.com>
2023-11-23 14:57:25 -0800

    task02

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit
+ * @n: parameter
+ * @index: parameter1
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+	bit_val = (n >> index) & 1;
+	return (bit_val);
+}
